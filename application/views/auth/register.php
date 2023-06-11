00d074a3986aff9c1341075c620f514b31ca3416
Thoriq Daffa <thordaf2305@gmail.com>
2023-06-11 10:10:04 +0700

    Add some page & Adjust some files

---
+++ application/views/auth/register.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <!-- Meta -->
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <!-- CSS -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+            <link rel="stylesheet" href="../../assets/css/auth.css">
+            <!-- Script JS -->
+            <script src="../../assets/js/slider.js"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+            <script src="https://kit.fontawesome.com/25af777db1.js" crossorigin="anonymous"></script>
+            <!--------------->
+            <title>Document</title>
+        </head>
+        <body>
+            <section class="auth">
+                <div class="d-flex flex-row">
+                    <div class="left-side">
+                        <div class="container-fluid left-img" style="background-color: #222222;width: 41em;height:100vh;">
+                            <div class="image"></div>
+                        </div>
+                    </div>
+                    <div class="right-side">
+                        <div class="container-fluid" style="width: 100vh;">
+                            <div class="body-text d-flex flex-column">
+                                <div class="title-page">
+                                    <h1>GBookIn</h1>
+                                </div>
+                                <div class="content-page">
+                                    <form action="" method="post">
+                                        <div class="form-login">
+                                            <div class="username">
+                                                <input type="text" placeholder="Input Username">
+                                            </div>
+                                            <div class="password">
+                                                <input type="password" name="password" id="password" placeholder="Input Password">
+                                            </div>
+                                            <div class="essential d-flex flex-row">
+                                                <div class="remember-input">
+                                                    <input type="checkbox" name="" id="checkbox">
+                                                    <label for="checkbox">Remember Me</label>
+                                                </div>
+                                                <div class="btn-forgot">
+                                                    <a href="" class="link">Forgot Password</a>
+                                                </div>
+                                            </div>
+                                            <div class="btn-auth">
+                                                <div class="login-btn">
+                                                    <button>Sign Up</button>
+                                                </div>
+                                                <div class="google-login">
+                                                    <a href="#" class="btn btn-sm">Google Account</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="footer-page d-flex flex-row">
+                                    <p>Have an account ?</p>
+                                    <a href="Login">Sign In</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </body>
+    </html>
