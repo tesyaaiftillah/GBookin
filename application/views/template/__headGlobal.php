c30f546d9bbd8922d2cd4afb0f2b43e1cd2748bf
Thoriq Daffa <thordaf2305@gmail.com>
2023-06-19 18:04:30 +0700

    Update

--- application/views/template/__headGlobal.php
+++ application/views/template/__headGlobal.php
@@ -10,7 +10,7 @@
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
             <link rel="stylesheet" href="../../asset/css/style.css">
             <!-- Script JS -->
-            <script src="../../../asset/js/navbar.js"></script>
+            <script src="../../../assets/js/navbar.js"></script>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
